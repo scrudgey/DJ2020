4ddcfcb34f461a95808f64babac8e67102cdbabd
RSHAW <scrudgey@gmail.com>
2021-08-05 21:33:16 -0700

    working clearsighter

--- ClearSighter.cs
+++ ClearSighter.cs
@@ -2,90 +2,106 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-class MaterialController {
+using UnityEngine.Rendering;
+public class MaterialController {
     public MeshRenderer renderer;
+    public GameObject gameObject;
     public float timer;
-    public MaterialController(MeshRenderer renderer) {
-        this.renderer = renderer;
+    public bool disableBecauseInterloper;
+    public bool disableBecauseAbove;
+    public MaterialController(GameObject gameObject) {
+        this.gameObject = gameObject;
+        this.renderer = gameObject.GetComponent<MeshRenderer>();
+    }
+    public void InterloperStart() {
+        timer = 1f;
+    }
+    public void CeilingCheck(Collider collider, Vector3 position) {
+        if (collider.bounds.center.y < position.y) {
+            // renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+            disableBecauseAbove = false;
+            return;
+        }
+        Vector3 otherFloor = collider.bounds.center - new Vector3(0f, collider.bounds.extents.y, 0f);
+        Vector3 direction = otherFloor - position;
+        if (direction.y > 2.0f) {
+            disableBecauseAbove = true;
+            // renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        } else {
+            disableBecauseAbove = false;
+            // renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
     }
     public void MakeTransparent() {
-        timer = 1f;
-        StandardShaderUtils.ChangeRenderMode(renderer.material, StandardShaderUtils.BlendMode.Transparent);
+        // StandardShaderUtils.ChangeRenderMode(renderer, StandardShaderUtils.BlendMode.Transparent);
+        if (renderer.shadowCastingMode != ShadowCastingMode.ShadowsOnly)
+            renderer.shadowCastingMode = ShadowCastingMode.ShadowsOnly;
+    }
+    public void MakeApparent() {
+        if (renderer.shadowCastingMode != ShadowCastingMode.On)
+            renderer.shadowCastingMode = ShadowCastingMode.On;
     }
     public void Update() {
-        timer -= Time.deltaTime;
+        // interloper logic
+        if (timer > 0)
+            timer -= Time.deltaTime;
         if (timer <= 0) {
-            StandardShaderUtils.ChangeRenderMode(renderer.material, StandardShaderUtils.BlendMode.Cutout);
+            disableBecauseInterloper = false;
+        } else {
+            disableBecauseInterloper = true;
+        }
+
+        if (active()) {
+            MakeTransparent();
+        } else {
+            MakeApparent();
+        }
+    }
+
+    public bool active() { return disableBecauseInterloper || disableBecauseAbove; }
+
+    // wallpress handling
+    // if (myCamera.state == CameraState.wallPress) {
+    //     renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    //     continue;
+    // }
+}
+public class MaterialControllerCache {
+    public Dictionary<GameObject, MaterialController> controllers = new Dictionary<GameObject, MaterialController>();
+    // public static Dictionary<GameObject, MeshRenderer> renderers = new Dictionary<GameObject, MeshRenderer>();
+    public MaterialController get(GameObject key) {
+        if (controllers.ContainsKey(key)) {
+            return controllers[key];
+        } else {
+            MaterialController controller = new MaterialController(key);
+            if (controller.renderer == null) {
+                Debug.Log($"{key} null controller");
+                controller = null;
+            }
+            controllers[key] = controller;
+            return controller;
         }
     }
 }
 public class ClearSighter : MonoBehaviour {
-    public static Dictionary<GameObject, MeshRenderer> renderers = new Dictionary<GameObject, MeshRenderer>();
-    // public static Dictionary<GameObject, TransparentMesh> transparentMeshes = new Dictionary<GameObject, TransparentMesh>();
-    static Dictionary<MeshRenderer, MaterialController> controllers = new Dictionary<MeshRenderer, MaterialController>();
+    public MaterialControllerCache controllers = new MaterialControllerCache();
     public NeoCharacterCamera myCamera;
     Transform myTransform;
-    public static MeshRenderer Renderer(GameObject key) {
-        if (renderers.ContainsKey(key)) {
-            return renderers[key];
-        } else {
-            MeshRenderer renderer = key.GetComponentInChildren<MeshRenderer>();
-            renderers[key] = renderer;
-            return renderer;
-        }
-    }
-    // public static TransparentMesh TransparentMaterial(GameObject inMaterial) {
-    //     if (transparentMeshes.ContainsKey(inMaterial)) {
-    //         return transparentMeshes[inMaterial];
-    //     } else {
-    //         TransparentMesh transparent = inMaterial.AddComponent<TransparentMesh>();
-    //         transparentMeshes[inMaterial] = transparent;
-    //         return transparent;
-    //     }
-    // }
     void Start() {
         myTransform = transform;
     }
 
     void Update() {
         // colliders above me
-        Collider[] others = Physics.OverlapSphere(transform.position, 20f);
+        Collider[] others = Physics.OverlapSphere(myTransform.position, 20f);
         foreach (Collider collider in others) {
             if (collider.transform.IsChildOf(myTransform))
                 continue;
-            MeshRenderer renderer = Renderer(collider.gameObject);
-            if (renderer == null)
-                continue;
-            if (myCamera.state == CameraState.wallPress) {
-                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-                continue;
-            }
-
-            if (collider.bounds.center.y < myTransform.position.y) {
-                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-                continue;
-            }
-
-            Vector3 otherFloor = collider.bounds.center - new Vector3(0f, collider.bounds.extents.y, 0f);
-            Vector3 direction = otherFloor - myTransform.position;
-            if (direction.y > 2.0f) {
-                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-            } else {
-                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-            }
+            MaterialController controller = controllers.get(collider.gameObject);
+            if (controller != null)
+                controller.CeilingCheck(collider, myTransform.position);
         }
 
-        // reenable things
-        HashSet<MeshRenderer> removeControllers = new HashSet<MeshRenderer>();
-        foreach (KeyValuePair<MeshRenderer, MaterialController> kvp in controllers) {
-            kvp.Value.Update();
-            if (kvp.Value.timer <= 0) {
-                removeControllers.Add(kvp.Key);
-            }
-        }
-        foreach (MeshRenderer key in removeControllers) {
-            controllers.Remove(key);
-        }
 
         // collider between me and the camera
         float distance = Vector3.Distance(myCamera.transform.position, transform.position) + 1f;
@@ -93,17 +109,24 @@ public class ClearSighter : MonoBehaviour {
             if (hit.collider.transform.IsChildOf(transform)) {
                 break;
             }
-            MeshRenderer meshRenderer = Renderer(hit.collider.gameObject);
-
-            if (meshRenderer != null) {
-                if (controllers.ContainsKey(meshRenderer)) {
-                    controllers[meshRenderer].MakeTransparent();
-                } else {
-                    MaterialController controller = new MaterialController(meshRenderer);
-                    controllers[meshRenderer] = controller;
-                    controller.MakeTransparent();
-                }
+            MaterialController controller = controllers.get(hit.collider.gameObject);
+            if (controller != null) {
+                controller.InterloperStart();
             }
+        }
+
+        // garbage collect
+        HashSet<MaterialController> removeControllers = new HashSet<MaterialController>();
+        foreach (MaterialController controller in controllers.controllers.Values) {
+            if (controller == null)
+                continue;
+            controller.Update();
+            if (!controller.active()) {
+                removeControllers.Add(controller);
+            }
+        }
+        foreach (MaterialController key in removeControllers) {
+            controllers.controllers.Remove(key.gameObject);
         }
     }
 }
--- StandardShaderUtils.cs
+++ StandardShaderUtils.cs
@@ -9,52 +9,63 @@ public static class StandardShaderUtils {
     }
 
     public static void ChangeRenderMode(Renderer renderer, BlendMode blendMode) {
+        MaterialPropertyBlock _propBlock = new MaterialPropertyBlock();
         switch (blendMode) {
             case BlendMode.Opaque:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                standardShaderMaterial.SetInt("_ZWrite", 1);
-                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = -1;
+                renderer.GetPropertyBlock(_propBlock);
+
+                _propBlock.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                _propBlock.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                _propBlock.SetInt("_ZWrite", 1);
+                // _propBlock.S
+                // _propBlock.DisableKeyword("_ALPHATEST_ON");
+                // _propBlock.DisableKeyword("_ALPHABLEND_ON");
+                // _propBlock.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                // _propBlock.renderQueue = -1;
+
+                renderer.SetPropertyBlock(_propBlock);
                 break;
             case BlendMode.Cutout:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                standardShaderMaterial.SetInt("_ZWrite", 1);
-                standardShaderMaterial.EnableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = 2450;
-                standardShaderMaterial.SetColor("_Color", Color.white);
+                renderer.GetPropertyBlock(_propBlock);
+
+                _propBlock.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                _propBlock.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                _propBlock.SetInt("_ZWrite", 1);
+                // _propBlock.EnableKeyword("_ALPHATEST_ON");
+                // _propBlock.DisableKeyword("_ALPHABLEND_ON");
+                // _propBlock.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                // _propBlock.renderQueue = 2450;
+                _propBlock.SetColor("_Color", Color.white);
+
+                renderer.SetPropertyBlock(_propBlock);
                 break;
             case BlendMode.Fade:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                standardShaderMaterial.SetInt("_ZWrite", 0);
-                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.EnableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = 3000;
+                renderer.GetPropertyBlock(_propBlock);
+
+                _propBlock.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                _propBlock.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                _propBlock.SetInt("_ZWrite", 0);
+                // _propBlock.DisableKeyword("_ALPHATEST_ON");
+                // _propBlock.EnableKeyword("_ALPHABLEND_ON");
+                // _propBlock.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                // _propBlock.renderQueue = 3000;
+                renderer.SetPropertyBlock(_propBlock);
                 break;
             case BlendMode.Transparent:
-                //     _renderer.GetPropertyBlock(_propBlock);
-                // // Assign our new value.
-                // _propBlock.SetColor("_Color", Color.Lerp(Color1, Color2, (Mathf.Sin(Time.time * Speed + Offset) + 1) / 2f));
-                // // Apply the edited values to the renderer.
-                // _renderer.SetPropertyBlock(_propBlock);
+                renderer.GetPropertyBlock(_propBlock);
 
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                standardShaderMaterial.SetInt("_ZWrite", 0);
-                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                _propBlock.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                _propBlock.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                _propBlock.SetInt("_ZWrite", 0);
+                // _propBlock.DisableKeyword("_ALPHATEST_ON");
+                // _propBlock.DisableKeyword("_ALPHABLEND_ON");
+                // _propBlock.EnableKeyword("_ALPHAPREMULTIPLY_ON");
                 Color newColor = Color.white;
                 newColor.a = 0.3f;
-                standardShaderMaterial.SetColor("_Color", newColor);
-                standardShaderMaterial.renderQueue = 3000;
+                _propBlock.SetColor("_Color", newColor);
+                // _propBlock.renderQueue = 3000;
+
+                renderer.SetPropertyBlock(_propBlock);
                 break;
         }
 
