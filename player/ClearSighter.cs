31cb9c78b77b1fd71779b563d8f59a233d1c5eea
Ryan Foltz <scrudgey@gmail.com>
2022-10-10 20:01:52 -0700

    subsidize clearsighter operation over several fram

--- player/ClearSighter.cs
+++ player/ClearSighter.cs
@@ -9,13 +9,14 @@ using UnityEngine.Rendering;
 public class ClearSighter : MonoBehaviour {
     public MaterialControllerCache controllers;
     public CharacterCamera myCamera;
-    public Shader normalShader;
-    public Shader interloperShader;
-    public Collider cylinderCollider;
+    // public Shader normalShader;
+    // public Shader interloperShader;
+    // public Collider cylinderCollider;
     public float floorHeight;
     Transform myTransform;
     public List<MaterialController> interlopers = new List<MaterialController>();
-    List<(MaterialController, Vector3)> staticGeometry = new List<(MaterialController, Vector3)>();
+    // List<(MaterialController, Vector3)> staticGeometry = new List<(MaterialController, Vector3)>();
+    Dictionary<MaterialController, Vector3> staticGeometry = new Dictionary<MaterialController, Vector3>();
     public Transform followTransform;
     private List<Collider> rooftopZones = new List<Collider>();
 
@@ -26,6 +27,89 @@ public class ClearSighter : MonoBehaviour {
         InitializeMaterialControllerCache();
         InvokeRepeating("HandleStaticGeometry", 0f, 1f);
         rooftopZones = GameObject.FindObjectsOfType<RooftopZone>().Select(zone => zone.GetComponent<Collider>()).ToList();
+        coroutine = StartCoroutine(RunJobRepeatedly());
+    }
+    IEnumerator RunJobRepeatedly() {
+        while (true) {
+            if (followTransform == null) {
+                yield return null;
+                continue;
+            }
+            int i = 0;
+            myTransform.position = followTransform.position;
+
+            inRooftopZone = false;
+            foreach (Collider zone in rooftopZones) {
+                if (zone.bounds.Contains(myTransform.position)) {
+                    inRooftopZone = true;
+                }
+            }
+
+            Vector3 directionToCamera = -1f * myCamera.transform.forward;
+            myTransform.rotation = Quaternion.LookRotation(directionToCamera);
+
+            // non-static colliders above me
+            Collider[] others = Physics.OverlapSphere(myTransform.position, 20f, LayerUtil.GetMask(Layer.obj, Layer.bulletPassThrough))//, Layer.shell))
+                .GroupBy(collider => collider.transform.root)
+                .Select(g => g.First())
+                .Where(collider =>
+                    collider != null &&
+                    collider.gameObject != null &&
+                    !collider.transform.IsChildOf(myTransform) &&
+                    !collider.transform.IsChildOf(followTransform))
+                // collider.tag != "shell")
+                .ToArray();
+            foreach (Collider collider in others) {
+                i += 1;
+                if (i > 500) {
+                    i = 0;
+                    yield return new WaitForEndOfFrame();
+                }
+                MaterialController controller = controllers.get(collider);
+                if (controller != null) {
+                    // controller.CeilingCheck(myTransform.position);
+                    if (inRooftopZone) {
+                        controller.disableBecauseAbove = false;
+                    } else {
+                        controller.CeilingCheck(myTransform.position);
+                    }
+                }
+            }
+
+            // interloper colliders
+            // Debug.Log($"interlopers: {interlopers.Count}");
+            foreach (MaterialController interloper in interlopers.Where(interloper =>
+                                                                        interloper != null &&
+                                                                        interloper.gameObject != null
+                                                                        )) {
+                i += 1;
+                if (i > 500) {
+                    i = 0;
+                    yield return new WaitForEndOfFrame();
+                }
+                Vector3 directionToInterloper = interloper.collider.bounds.center - myTransform.position;
+                if (Vector3.Dot(directionToCamera, directionToInterloper) > 0 && directionToInterloper.y > -0.01f)
+                    interloper.InterloperStart();
+
+                Vector3 directionToMesh = interloper.collider.bounds.center - myTransform.position;
+                float axialDistance = Vector3.Dot(directionToMesh, directionToCamera);
+                Vector3 offAxis = directionToMesh - (axialDistance * directionToCamera);
+                float offAxisLength = offAxis.magnitude;
+                interloper.UpdateTargetAlpha(offAxisLength: offAxisLength);
+                interloper.Update();
+            }
+            var nonInterlopers = controllers.controllers.Values.Where(controller => controller != null && !interlopers.Contains(controller)).ToList();
+            foreach (MaterialController controller in nonInterlopers) {
+                i += 1;
+                if (i > 500) {
+                    i = 0;
+                    yield return new WaitForEndOfFrame();
+                }
+                controller.UpdateTargetAlpha(0);
+                controller.Update();
+            }
+            yield return null;
+        }
     }
     void InitializeMaterialControllerCache() {
         CharacterCamera cam = GameObject.FindObjectOfType<CharacterCamera>();
@@ -42,7 +126,7 @@ public class ClearSighter : MonoBehaviour {
                 continue;
             MaterialController controller = controllers.get(collider);
             if (collider.gameObject.isStatic) {
-                staticGeometry.Add((controller, collider.transform.position));
+                staticGeometry[controller] = collider.transform.position;
             }
         }
         Debug.Log($"[ClearSighter] initialized material controller cache with {controllers.controllers.Count}");
@@ -50,8 +134,7 @@ public class ClearSighter : MonoBehaviour {
 
     void HandleStaticGeometry() {
         Vector3 myPosition = myTransform.position;
-
-        foreach ((MaterialController controller, Vector3 position) in staticGeometry.Where(x => x.Item2.y > floorHeight)) {
+        foreach ((MaterialController controller, Vector3 position) in staticGeometry) {
             if (controller == null || controller.gameObject == null || controller.collider == null || !controller.gameObject.activeInHierarchy)
                 continue;
             if (inRooftopZone) {
@@ -59,72 +142,6 @@ public class ClearSighter : MonoBehaviour {
             } else {
                 controller.CeilingCheck(myPosition);
             }
-            controller.UpdateTargetAlpha(0);
-            controller.Update();
-        }
-    }
-    void LateUpdate() {
-        ProcessAllUpdates();
-    }
-    void ProcessAllUpdates() {
-        if (followTransform == null)
-            return;
-        myTransform.position = followTransform.position;
-
-        inRooftopZone = false;
-        foreach (Collider zone in rooftopZones) {
-            if (zone.bounds.Contains(myTransform.position)) {
-                inRooftopZone = true;
-            }
-        }
-
-        Vector3 directionToCamera = -1f * myCamera.transform.forward;
-        myTransform.rotation = Quaternion.LookRotation(directionToCamera);
-
-        // colliders above me
-        Collider[] others = Physics.OverlapSphere(myTransform.position, 20f, LayerUtil.GetMask(Layer.obj, Layer.bulletPassThrough))//, Layer.shell))
-            .GroupBy(collider => collider.transform.root)
-            .Select(g => g.First())
-            .Where(collider =>
-                collider != null &&
-                collider.gameObject != null &&
-                !collider.transform.IsChildOf(myTransform) &&
-                !collider.transform.IsChildOf(followTransform))
-            // collider.tag != "shell")
-            .ToArray();
-        foreach (Collider collider in others) {
-            MaterialController controller = controllers.get(collider);
-            if (controller != null) {
-                // controller.CeilingCheck(myTransform.position);
-                if (inRooftopZone) {
-                    controller.disableBecauseAbove = false;
-                } else {
-                    controller.CeilingCheck(myTransform.position);
-                }
-            }
-        }
-
-        // interloper colliders
-        // Debug.Log($"interlopers: {interlopers.Count}");
-        foreach (MaterialController interloper in interlopers.Where(interloper =>
-                                                                    interloper != null &&
-                                                                    interloper.gameObject != null
-                                                                    )) {
-            Vector3 directionToInterloper = interloper.collider.bounds.center - myTransform.position;
-            if (Vector3.Dot(directionToCamera, directionToInterloper) > 0 && directionToInterloper.y > -0.01f)
-                interloper.InterloperStart();
-
-            Vector3 directionToMesh = interloper.collider.bounds.center - myTransform.position;
-            float axialDistance = Vector3.Dot(directionToMesh, directionToCamera);
-            Vector3 offAxis = directionToMesh - (axialDistance * directionToCamera);
-            float offAxisLength = offAxis.magnitude;
-            interloper.UpdateTargetAlpha(offAxisLength: offAxisLength);
-            interloper.Update();
-        }
-        foreach (MaterialController controller in controllers.controllers.Values.Where(controller =>
-                                                                                        controller != null &&
-                                                                                        !interlopers.Contains(controller))) {
-            controller.UpdateTargetAlpha(0);
             controller.Update();
         }
     }
